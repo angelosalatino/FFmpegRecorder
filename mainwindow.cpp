814ca64499d6c16e66ce47a80c045d837f755c4c
Angelo Antonio Salatino <angelosalatino@LabFIT01.(none)>
2014-02-10 19:47:10 +0100

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("FFmpeg Recorder");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
